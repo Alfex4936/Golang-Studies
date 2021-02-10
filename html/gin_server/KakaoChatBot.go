451c5e8197f264539fa88433b57a03b35b85e3ee
Seok Won <ikr@kakao.com>
2021-02-10 10:17:37 +0900

    카카오 챗봇 /last POST 테스트

--- html/gin_server/KakaoChatBot.go
+++ html/gin_server/KakaoChatBot.go
@@ -3,9 +3,59 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
+	"strconv"
+	"strings"
 
+	"github.com/anaskhan96/soup"
 	"github.com/gin-gonic/gin"
 )
+
+const ajouLink = "https://www.ajou.ac.kr/kr/ajou/notice.do"
+
+// Notice {id: int, title: str, date: str, link: str, writer: str}
+type Notice struct {
+	id     int64
+	title  string
+	date   string
+	link   string
+	writer string
+}
+
+// Parse is a function that parses a length of notices
+func Parse(length int) []Notice {
+	ajouHTML := fmt.Sprintf("%v?mode=list&articleLimit=%v&article.offset=0", ajouLink, length)
+
+	resp, err := soup.Get(ajouHTML)
+	if err != nil {
+		fmt.Println("Check your HTML connection.")
+		os.Exit(2)
+	}
+	doc := soup.HTMLParse(resp)
+
+	notices := []Notice{}
+	ids := doc.FindAll("td", "class", "b-num-box")
+	if len(ids) == 0 {
+		fmt.Println("Check your parser.")
+		os.Exit(2)
+	}
+
+	titles := doc.FindAll("div", "class", "b-title-box")
+	dates := doc.FindAll("span", "class", "b-date")
+	//links := doc.FindAll("div", "class", "b-title-box")
+	writers := doc.FindAll("span", "class", "b-writer")
+	for i := 0; i < length; i++ {
+		id, _ := strconv.ParseInt(strings.TrimSpace(ids[i].Text()), 10, 64)
+		title := strings.TrimSpace(titles[i].Find("a").Text())
+		link := titles[i].Find("a").Attrs()["href"]
+		date := strings.TrimSpace(dates[i].Text())
+		writer := writers[i].Text()
+		notice := Notice{id: id, title: title, date: date, link: ajouLink + link, writer: writer}
+		notices = append(notices, notice)
+	}
+
+	return notices
+}
 
 // SimpleText for Kakao Response
 type SimpleText struct {
@@ -85,6 +135,36 @@ func main() {
 		})
 	})
 
+	router.POST("/last", func(c *gin.Context) {
+		var kjson KakaoJSON
+		if err := c.BindJSON(&kjson); err != nil {
+			errorMsg := SimpleText{Version: "2.0"}
+			errorMsg.Template.Outputs.SimpleText.Text = err.Error()
+			c.JSON(http.StatusBadRequest, errorMsg)
+			return
+		}
+
+		notice := Parse(1)[0]
+		noticeJSON := gin.H{"title": notice.title, "description": notice.writer, "link": gin.H{"web": notice.link}}
+
+		// Card
+		items := []gin.H{}
+		buttons := []gin.H{}
+		header := gin.H{"title": "header title"}
+
+		// Add one care item
+		items = append(items, noticeJSON)
+		buttons = append(buttons, gin.H{"label": "공유하기", "action": "share"})
+
+		// Make a template
+		template := gin.H{"outputs": []gin.H{{"listCard": gin.H{"header": header, "items": items, "buttons": buttons}}}}
+
+		listCard := gin.H{"version": "2.0", "template": template}
+		c.PureJSON(http.StatusOK, listCard)
+		// c.JSON(http.StatusOK, listCard)
+
+	})
+
 	router.POST("/json", func(c *gin.Context) {
 		var kjson KakaoJSON
 		if err := c.BindJSON(&kjson); err != nil {
@@ -133,8 +213,7 @@ func main() {
 		item2 := gin.H{"title": "card2", "description": "desc2"}
 
 		// Add two cards
-		items = append(items, item)
-		items = append(items, item2)
+		items = append(items, item, item2)
 
 		// QuickReplies [Optional]
 		quickReplies := []gin.H{}
@@ -144,11 +223,18 @@ func main() {
 		quickReplies = append(quickReplies, quickReply)
 
 		// Make a template
-		template := gin.H{"outputs": []gin.H{gin.H{"listCard": gin.H{"header": header, "items": items}}}}
+		template := gin.H{"outputs": []gin.H{{"listCard": gin.H{"header": header, "items": items}}}}
 		template["quickReplies"] = quickReplies // Optional
 		listCard := gin.H{"version": "2.0", "template": template}
 
-		c.JSON(http.StatusOK, listCard)
+		// buf := new(bytes.Buffer)
+		// enc := json.NewEncoder(buf)
+		// enc.SetEscapeHTML(false)
+		// if err := enc.Encode(&listCard); err != nil {
+		// 	log.Println(err)
+		// }
+
+		c.PureJSON(http.StatusOK, listCard)
 	})
 
 	router.Run(":8000")
